346100933cb33f680f66ae8719679365c2e95d9a
Luke Shen <yipingcool@gmail.com>
2023-09-08 16:23:26 +1000

    first commit

---
+++ e2e/starter.test.js
@@ -0,0 +1,58 @@
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp
+    ({
+      newInstance: true,
+    });
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+/* eslint-env detox/detox */
+
+
+  it('should show "Step One" at the begging', async () => {
+    await expect(element(by.text('Step One'))).toBeVisible();
+  });
+
+  it('should render "See Your Changes" in the second slide', async () => {
+    await element(by.id('slides')).swipe('left');
+    await expect(element(by.text('See Your Changes'))).toBeVisible();
+  });
+
+  it('should enable swiping back and forth', async () => {
+    await expect(element(by.text('Step One'))).toBeVisible();
+    await element(by.id('slides')).swipe('left');
+    await element(by.id('slides')).swipe('right');
+    await expect(element(by.text('Step One'))).toBeVisible();
+  });
+
+  it('should render "Debug" and have a Button to click in the third slide', async () => {
+    await element(by.id('slides')).swipe('left');
+    await element(by.id('slides')).swipe('left');
+    await expect(element(by.text('Debug'))).toBeVisible();
+
+    await element(by.text('Click here!')).tap();
+    await expect(element(by.text('Clicked!'))).toBeVisible();
+  });
+
+  it('should render "Learn More" and change text in the fourth slide', async () => {
+    await element(by.id('slides')).swipe('left');
+    await element(by.id('slides')).swipe('left');
+    await element(by.id('slides')).swipe('left');
+    await expect(element(by.text('Learn More'))).toBeVisible();
+
+    const docsInput = element(by.id('docsInput'));
+
+    await expect(docsInput).toBeVisible();
+
+    await docsInput.clearText();
+    await docsInput.typeText('Maybe later!');
+
+    await expect(docsInput).toHaveText('Maybe later!');
+  });
+
+
+});
